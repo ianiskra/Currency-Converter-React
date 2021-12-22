8705d5f70a633336456cfb052190dbb05e17c467
Ian Iskra <ianiskra@gmail.com>
2021-12-21 19:28:16 -0800

    Added country flags png, added style to CurrencySearch Layout

--- src/components/CurrencySearch.js
+++ src/components/CurrencySearch.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './CurrencySearch.css';
 import { currencyList } from '../utils/currencyList';
+import aud from '../flags/aud.png';
+import bdt from '../flags/bdt.png';
+import bif from '../flags/bif.png';
+import etb from '../flags/etb.png';
+import ils from '../flags/ils.png';
+import mdl from '../flags/mdl.png';
 
 export const CurrencySearch = () => {
 
@@ -21,13 +27,14 @@ export const CurrencySearch = () => {
         let currencyFetch = await fetch(url);
         let response = await currencyFetch.json();
         console.log('currency', response.data);
+        
         setSelectedCountries({
-            AUD: parseFloat(response.data.AUD) * parseFloat(conversionRate),
-            BDT: response.data.BDT,
-            BIF: response.data.BIF,
-            ETB: response.data.ETB,
-            ILS: response.data.ILS,
-            MDL: response.data.MDL,
+            AUD: parseFloat(response.data.AUD).toFixed(2) * parseFloat(conversionRate),
+            BDT: parseFloat(response.data.BDT).toFixed(2) * parseFloat(conversionRate),
+            BIF: parseFloat(response.data.BIF).toFixed(2) * parseFloat(conversionRate),
+            ETB: parseFloat(response.data.ETB).toFixed(2) * parseFloat(conversionRate),
+            ILS: parseFloat(response.data.ILS).toFixed(2) * parseFloat(conversionRate),
+            MDL: parseFloat(response.data.MDL).toFixed(2) * parseFloat(conversionRate),
         });
         setCurrency(response.data);
         return response.data;
@@ -36,19 +43,19 @@ export const CurrencySearch = () => {
     useEffect(() => {
         // comparing a country with other countries
         fetchCurrency(country);
-    }, [country])
+    }, [country, conversionRate])
 
     return (
         <div className="page-container">
-            <h1>Currency Search</h1>
+            <h1 style={{color: "white"}} >Currency Search</h1>
             <div className="currency-container">
                 {/* {Object.keys(currency).length > 0 ? currency.AFN : "No Data"} */}
 
                 {/* Dropdown list of currencies */}
-                <div className="currency-search">
+                <div id="conversion" className="currency-search">
                     <div>
                         <h2>Conversion Rate</h2>
-                        <input type="text" value={conversionRate} onChange={(e) => {
+                        <input id="conversion-box" type="text" value={conversionRate} onChange={(e) => {
                             setConversionRate(e.target.value);
                         }} />
                     </div>
@@ -69,28 +76,31 @@ export const CurrencySearch = () => {
 
                 <div className="currency-search">
                     <div className="countries">
-                        <span>AUD</span> {selectedCountries.AUD != null ? selectedCountries.AUD : ""}
+                        <img src={aud} alt="Australia" />
+                        <span>AUD {selectedCountries.AUD != null ? selectedCountries.AUD : ""} </span>
                     </div>
                     <div className="countries">
-                        <span>BDT</span> {selectedCountries.BDT != null ? selectedCountries.BDT : ""}
+                        <img src={bdt} alt="Bangladesh" />
+                        <span>BDT {selectedCountries.BDT != null ? selectedCountries.BDT : ""} </span>
                     </div>
                     <div className="countries">
-                        <span>BIF</span> {selectedCountries.BIF != null ? selectedCountries.BIF : ""}
+                        <img src={bif} alt="Burundia" />
+                        <span>BIF {selectedCountries.BIF != null ? selectedCountries.BIF : ""} </span>
                     </div>
                     <div className="countries">
-                        <span>ETB</span> {selectedCountries.ETB != null ? selectedCountries.ETB : ""}
+                        <img src={etb} alt="Ethopia" />
+                        <span>ETB {selectedCountries.ETB != null ? selectedCountries.ETB : ""} </span>
                     </div>
                     <div className="countries">
-                        <span>ILS</span> {selectedCountries.ILS != null ? selectedCountries.ILS : ""}
+                        <img src={ils} alt="Israel" />
+                        <span>ILS {selectedCountries.ILS != null ? selectedCountries.ILS : ""} </span>
                     </div>
                     <div className="countries">
-                        <span>MDL</span> {selectedCountries.MDL != null ? selectedCountries.MDL : ""}
+                        <img src={mdl} alt="Moldova" />
+                        <span>MDL {selectedCountries.MDL != null ? selectedCountries.MDL : ""} </span>
                     </div>
                 </div>
 
-                <div className="currency-search">
-                    <h2>Hello WOrld</h2>
-                </div>
             </div>
         </div>
     )
